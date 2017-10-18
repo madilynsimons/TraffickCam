fa4509c898a20eea8470e4f612ba20cb4d2d361b
Madilyn Simons <madilynsimons@gmail.com>
2017-10-18 01:14:22 -0400

    Starting to implement Google Places API

--- app/src/main/java/com/example/owner/traffickcam/MainActivity.java
+++ app/src/main/java/com/example/owner/traffickcam/MainActivity.java
@@ -1,43 +1,31 @@
 package com.example.owner.traffickcam;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.File;
-
-import io.fotoapparat.Fotoapparat;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlacePicker;
 
 public class MainActivity extends AppCompatActivity {
-
-    String default_hotel;
-    String default_room;
-    EditText rn;
-    EditText hn;
-
-    static final int REQUEST_IMAGE_CAPTURE = 1;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        rn = (EditText) findViewById(R.id.room_number);
-        hn = (EditText) findViewById(R.id.hotel_name);
-
-        default_hotel = this.getResources().getString(R.string.default_hotel);
-        default_room = this.getResources().getString(R.string.default_room);
-    }
 
     /** called when user presses button **/
     public void startCamera(View view)
     {
-        boolean valid = isValidHotel();
-        valid = true;
+        boolean valid = true;
         if(! valid )
         {
             Toast.makeText(this, R.string.invalid_hotel_input, Toast.LENGTH_LONG).show();
@@ -50,17 +38,10 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private boolean isValidHotel()
-    {
-        String room_number = rn.getText().toString();
-        String hotel_name = hn.getText().toString();
-
-        if(room_number.equals(default_room) || hotel_name.equals(default_hotel))
-        {
-            return false;
-        }
-        return true;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
-
 
 }
