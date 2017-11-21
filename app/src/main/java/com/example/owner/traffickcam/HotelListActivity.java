6534f8761289dbf663a6daef12a98a18bb984c7e
Madilyn Simons <madilynsimons@gmail.com>
2017-11-20 21:55:39 -0500

    Working ListView Activity

---
+++ app/src/main/java/com/example/owner/traffickcam/HotelListActivity.java
@@ -0,0 +1,202 @@
+package com.example.owner.traffickcam;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Point;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.Projection;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.stream.JsonReader;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class HotelListActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        com.google.android.gms.location.LocationListener {
+
+    ListView mList;
+    List<String> nearbyHotels;
+    private ArrayAdapter<String> mAdapter;
+    int PROXIMITY_RADIUS = 10000;
+    double latitude = 39.981,longitude = -75.156;
+    String url;
+    Context context;
+    private GoogleApiClient client;
+    private LocationRequest locationRequest;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hotel_list);
+        mList = (ListView) findViewById(R.id.hotel_list);
+        nearbyHotels = new ArrayList<String>();
+        context = this;
+        printText("okay");
+    }
+
+    // TODO -- this is temp
+    public void tempButton(View view)
+    {
+        searchNearbyHotels();
+    }
+
+    private void searchNearbyHotels()
+    {
+        HotelData getNearbyPlacesData = new HotelData();
+
+        url = getUrl(latitude, longitude, "lodging");
+
+        getNearbyPlacesData.execute();
+    }
+
+    private String getUrl(double latitude , double longitude , String nearbyPlace)
+    {
+
+        StringBuilder googlePlaceUrl = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
+        googlePlaceUrl.append("location="+latitude+","+longitude);
+        googlePlaceUrl.append("&radius="+PROXIMITY_RADIUS);
+        googlePlaceUrl.append("&type="+nearbyPlace);
+        googlePlaceUrl.append("&sensor=true");
+        googlePlaceUrl.append("&key="+"AIzaSyBfIXGXMiK1NIS6bxcVJxN6R-n_gZFfvTM");
+
+        Log.d("MapsActivity", "url = "+googlePlaceUrl.toString());
+
+        return googlePlaceUrl.toString();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        locationRequest = new LocationRequest();
+        locationRequest.setInterval(100);
+        locationRequest.setFastestInterval(1000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION ) == PackageManager.PERMISSION_GRANTED)
+        {
+            LocationServices.FusedLocationApi.requestLocationUpdates(client, locationRequest, this);
+            buildGoogleApiClient();
+
+            LocationManager locationManager = (LocationManager)
+                    getSystemService(Context.LOCATION_SERVICE);
+            Criteria criteria = new Criteria();
+
+            Location location = locationManager.getLastKnownLocation(locationManager
+                    .getBestProvider(criteria, false));
+            latitude = location.getLatitude();
+            longitude = location.getLongitude();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+        if(client != null)
+        {
+            LocationServices.FusedLocationApi.removeLocationUpdates(client,this);
+        }
+    }
+
+    public void printText(String text)
+    {
+        Toast.makeText(this, text, Toast.LENGTH_LONG).show();
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        client = new GoogleApiClient.Builder(this).addConnectionCallbacks(this).addOnConnectionFailedListener(this).addApi(LocationServices.API).build();
+        client.connect();
+
+    }
+
+    public class HotelData extends AsyncTask<Object, String, String> {
+
+        private String googlePlacesData;
+        public List<HashMap<String, String>> nearbyPlaceList;
+
+        @Override
+        protected String doInBackground(Object... objects)
+        {
+            DownloadURL downloadURL = new DownloadURL();
+            try
+            {
+                googlePlacesData = downloadURL.readUrl(url);
+            }catch(IOException e)
+            {
+                // TODO
+            }
+            return googlePlacesData;
+        }
+
+        @Override
+        protected void onPostExecute(String s){
+            DataParser parser = new DataParser();
+            nearbyPlaceList = parser.parse(s);
+            showNearbyPlaces(nearbyPlaceList);
+        }
+
+        private void showNearbyPlaces(List<HashMap<String, String>> nearbyPlaceList)
+        {
+            printText(""+nearbyPlaceList.size());
+            for(int i = 0; i < nearbyPlaceList.size(); i++)
+            {
+                HashMap<String, String> googlePlace = nearbyPlaceList.get(i);
+
+                String placeName = googlePlace.get("place_name");
+                String vicinity = googlePlace.get("vicinity");
+                nearbyHotels.add(placeName);
+                printText(placeName);
+            }
+            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
+                    (context, android.R.layout.simple_list_item_1, nearbyHotels);
+            mList.setAdapter(arrayAdapter);
+        }
+    }
+}
--- app/src/main/java/com/example/owner/traffickcam/MainActivity.java
+++ app/src/main/java/com/example/owner/traffickcam/MainActivity.java
@@ -26,7 +26,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       Intent intent = new Intent(this, TraffickCamFotoActivity.class);
+       //Intent intent = new Intent(this, TraffickCamFotoActivity.class);
+        Intent intent = new Intent(this, HotelListActivity.class);
         startActivity(intent);
     }
 
